b11c3c5b958ba466dfb68ed4f4b53bb800f3f292
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-03 14:19:19 +0530

    intergrate with tailwind

--- weather-app/src/renderer/src/App.jsx
+++ weather-app/src/renderer/src/App.jsx
@@ -1,12 +1,13 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
+import Header from './components/Header'
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
